bc9b2bfe68fc39747ab56f074e102a3a75edf22a
Eliot Shepard <eshepard@slower.net>
2016-11-17 10:35:39 -0500

    initial release

--- bot.rb
+++ bot.rb
@@ -4,6 +4,8 @@ require 'logging'
 logger = Logging.logger(STDOUT)
 logger.level = :debug
 
+EMOJI = %w(:nail_care: :speech_balloon: :boom: :eyes: :floppy_disk: :e-mail: :sexynam: :sparkling_heart:)
+
 Slack.configure do |config|
   config.token = ENV['SLACK_TOKEN']
   if not config.token
@@ -22,10 +24,10 @@ end
 # listen for channel_joined event - https://api.slack.com/events/channel_joined
 client.on :channel_joined do |data|
   if joiner_is_bot?(client, data)
-    client.message channel: data['channel']['id'], text: "Thanks for the invite! I don\'t do much yet, but #{help}"
+    # client.message channel: data['channel']['id'], text: "Thanks for the invite! I don\'t do much yet, but #{help}"
     logger.debug("#{client.self['name']} joined channel #{data['channel']['id']}")
   else
-    logger.debug("Someone far less important than #{client.self['name']} joined #{data['channel']['id']}")
+    # logger.debug("Someone far less important than #{client.self['name']} joined #{data['channel']['id']}")
   end
 end
 
@@ -33,39 +35,48 @@ end
 client.on :message do |data|
 
   case data['text']
-  when 'hi', 'bot hi' then
-    client.typing channel: data['channel']
-    client.message channel: data['channel'], text: "Hello <@#{data['user']}>."
-    logger.debug("<@#{data['user']}> said hi")
+  # when 'hi', 'bot hi' then
+  #   client.typing channel: data['channel']
+  #   client.message channel: data['channel'], text: "Hello <@#{data['user']}>."
+  #   logger.debug("<@#{data['user']}> said hi")
 
-    if direct_message?(data)
-      client.message channel: data['channel'], text: "It\'s nice to talk to you directly."
-      logger.debug("And it was a direct message")
-    end
+  #   if direct_message?(data)
+  #     client.message channel: data['channel'], text: "It\'s nice to talk to you directly."
+  #     logger.debug("And it was a direct message")
+  #   end
 
-  when 'attachment', 'bot attachment' then
-    # attachment messages require using web_client
-    client.web_client.chat_postMessage(post_message_payload(data))
-    logger.debug("Attachment message posted")
+  # when 'attachment', 'bot attachment' then
+  #   # attachment messages require using web_client
+  #   client.web_client.chat_postMessage(post_message_payload(data))
+  #   logger.debug("Attachment message posted")
 
   when bot_mentioned(client)
-    client.message channel: data['channel'], text: 'You really do care about me. :heart:'
+    client.message channel: data['channel'], text: EMOJI.sample
     logger.debug("Bot mentioned in channel #{data['channel']}")
 
-  when 'bot help', 'help' then
-    client.message channel: data['channel'], text: help
-    logger.debug("A call for help")
+  # when 'bot help', 'help' then
+  #   client.message channel: data['channel'], text: help
+  #   logger.debug("A call for help")
 
-  when /^bot/ then
-    client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, I don\'t understand. \n#{help}"
-    logger.debug("Unknown command")
+  # when /^bot/ then
+  #   client.message channel: data['channel'], text: "Sorry <@#{data['user']}>, I don\'t understand. \n#{help}"
+  #   logger.debug("Unknown command")
+
+  when /([A-Z]{2,4}-[0-9]{1,4})+/ then
+    if data['text'] !~ /https:\/\//
+      data['text'].scan(/([A-Z]{2,4}-[0-9]{1,4})/).flatten.each do |match|
+        client.message channel: data['channel'], text: "https://vicedev.atlassian.net/browse/#{match}"
+        logger.debug("Bot linked issue #{match} in #{data['channel']}")
+      end
+    end
   end
+
 end
 
-def direct_message?(data)
-  # direct message channles start with a 'D'
-  data['channel'][0] == 'D'
-end
+# def direct_message?(data)
+#   # direct message channles start with a 'D'
+#   data['channel'][0] == 'D'
+# end
 
 def bot_mentioned(client)
   # match on any instances of `<@bot_id>` in the message
@@ -77,30 +88,30 @@ def joiner_is_bot?(client, data)
 end
 
 def help
-  %Q(I will respond to the following messages: \n
-      `bot hi` for a simple message.\n
-      `bot attachment` to see a Slack attachment message.\n
-      `@<your bot\'s name>` to demonstrate detecting a mention.\n
-      `bot help` to see this again.)
+  # %Q(I will respond to the following messages: \n
+  #     `bot hi` for a simple message.\n
+  #     `bot attachment` to see a Slack attachment message.\n
+  #     `@<your bot\'s name>` to demonstrate detecting a mention.\n
+  #     `bot help` to see this again.)
 end
 
-def post_message_payload(data)
-  main_msg = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  {
-    channel: data['channel'],
-      as_user: true,
-      attachments: [
-        {
-          fallback: main_msg,
-          pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-          title: 'Host, deploy and share your bot in seconds.',
-          image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-          title_link: 'https://beepboophq.com/',
-          text: main_msg,
-          color: '#7CD197'
-        }
-      ]
-  }
-end
+# def post_message_payload(data)
+#   main_msg = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+#   {
+#     channel: data['channel'],
+#       as_user: true,
+#       attachments: [
+#         {
+#           fallback: main_msg,
+#           pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+#           title: 'Host, deploy and share your bot in seconds.',
+#           image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+#           title_link: 'https://beepboophq.com/',
+#           text: main_msg,
+#           color: '#7CD197'
+#         }
+#       ]
+#   }
+# end
 
 client.start!
